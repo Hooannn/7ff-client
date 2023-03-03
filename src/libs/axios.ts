b4d7a0c6d5cdd432285dd7afd819830ee2b70ae8
Hooannn <hoanthui123@gmail.com>
2023-03-03 21:58:51 +0700

    feat: add refresh token flow

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,54 +1,75 @@
-import axios from "axios";
-import { getI18n } from "react-i18next";
+import axios from 'axios';
+import { getI18n } from 'react-i18next';
+import cookies from './cookies';
 export const axiosIns = axios.create({
   baseURL: import.meta.env.VITE_API_ENDPOINT,
   headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
   },
 });
 
-const getAccessToken = async () => {
-  // get access token here
-  return "";
-};
+const getAccessToken = () => cookies.get('access_token') || localStorage.getItem('access_token');
 
-const getRefreshToken = async () => {
-  // get refresh token here
-  return "";
-};
+const refreshToken = async () =>
+  new Promise<string | null>((resolve, reject) => {
+    axiosIns({
+      url: '/auth/refresh',
+      method: 'POST',
+      validateStatus: null,
+      data: {
+        accessToken: getAccessToken(),
+        refreshToken: cookies.get('refresh_token') || localStorage.getItem('refresh_token'),
+      },
+    })
+      .then(res => {
+        const token = res.data?.data;
+        if (token) {
+          cookies.set('access_token', token);
+          resolve(token);
+        } else {
+          window.location.href = '/auth';
+          resolve(null);
+        }
+      })
+      .catch(error => {
+        window.location.href = '/auth';
+        reject(error);
+      });
+  });
 
 axiosIns.interceptors.request.use(
-  async (config) => {
-    if (!config.headers["Authorization"]) {
-      const token = await getAccessToken();
-      config.headers["Authorization"] = `Bearer ${token}`;
+  async config => {
+    if (!config.headers['Authorization']) {
+      const token = getAccessToken();
+      config.headers['Authorization'] = `Bearer ${token}`;
     }
     const i18n = getI18n();
     if (config.params) config.params.locale = i18n.resolvedLanguage;
     else config.params = { locale: i18n.resolvedLanguage };
     return config;
   },
-  (error) => {
+  error => {
     return Promise.reject(error);
-  }
+  },
 );
 
 axiosIns.interceptors.response.use(
-  (response) => response,
-  async (error) => {
+  response => response,
+  async error => {
     const prevRequest = error?.config;
     if (error?.response?.status === 401 && !prevRequest?.sent) {
       prevRequest.sent = true;
-      const idToken = await getRefreshToken();
-      prevRequest.headers.Authorization = `Bearer ${idToken}`;
+      const token = await refreshToken();
+      if (!token) throw new Error('REFRESH_FAILED');
+      prevRequest.headers.Authorization = `Bearer ${token}`;
       return axiosIns({
         ...prevRequest,
         headers: prevRequest.headers.toJSON(),
       });
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 export default axiosIns;
