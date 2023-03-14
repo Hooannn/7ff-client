43f7e4b202dd18062e363ad0c98c5a4917f1baa9
Hooannn <hoanthui123@gmail.com>
2023-03-14 22:19:08 +0700

    feat: add auth protected component

--- src/@core/router/DashboardRouter.tsx
+++ src/@core/router/DashboardRouter.tsx
@@ -2,13 +2,13 @@ import { Suspense } from 'react';
 import DashboardLayout from '../../layouts/Dashboard';
 import DashboardPage from '../../pages/DashboardPage';
 import ErrorPage from '../../pages/ErrorPage';
-
+import AuthProtected from '../../components/AuthProtected';
 const dashboardRouter = [
   {
     path: '/dashboard',
     element: (
       <Suspense>
-        <DashboardLayout />
+        <AuthProtected children={<DashboardLayout />} redirect="/auth" adminOnly />
       </Suspense>
     ),
     errorElement: <ErrorPage />,
