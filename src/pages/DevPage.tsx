7ab85060d6460b2168656fd459ee7f98482a9fb5
Hooannn <hoanthui123@gmail.com>
2023-03-19 01:30:03 +0700

    feat: upload image example

--- src/pages/DevPage.tsx
+++ src/pages/DevPage.tsx
@@ -1,5 +1,24 @@
-import React from "react";
-
+import { UploadOutlined } from '@ant-design/icons';
+import { Row, Col, Button, Upload } from 'antd';
+import { secondaryButtonStyle } from '../assets/styles/globalStyle';
 export default function DevPage() {
-  return <div>DevPage</div>;
+  const productEndpoint = 'https://sevenfastfood-be.onrender.com/files/upload/image/single?folder=dev';
+  const devEnpoint = 'http://localhost:5000/files/upload/image/single?folder=dev';
+  return (
+    <Row style={{ padding: '28px' }}>
+      <Col span={24}>
+        <h3>Examples Component</h3>
+      </Col>
+      <Col span={24}>
+        <h4>Upload images</h4>
+        <Upload multiple accept="image/*" listType="picture" action={productEndpoint}>
+          <div style={{ width: '200px' }}>
+            <Button block shape="round" style={secondaryButtonStyle} icon={<UploadOutlined style={{ marginRight: '4px' }} />}>
+              <strong>Upload</strong>
+            </Button>
+          </div>
+        </Upload>
+      </Col>
+    </Row>
+  );
 }
