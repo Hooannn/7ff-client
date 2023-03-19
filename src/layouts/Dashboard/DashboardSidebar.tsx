a072538c488cce4833ccac48321d2ec740bb825c
YGOhappy123 <hagiahuy21052003@gmail.com>
2023-03-20 01:30:10 +0700

    add appBar component

--- src/layouts/Dashboard/DashboardSidebar.tsx
+++ src/layouts/Dashboard/DashboardSidebar.tsx
@@ -14,80 +14,14 @@ import {
   AreaChartOutlined,
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-
-const DASHBOARD_MENU_ITEMS = [
-  {
-    label: 'Dashboard',
-    key: '',
-    icon: <HomeOutlined />,
-  },
-  {
-    label: 'ClientFacing',
-    key: 'client_facing',
-    icon: <UserOutlined />,
-    children: [
-      {
-        label: 'Products',
-        key: 'products',
-        icon: <ShoppingCartOutlined />,
-      },
-      {
-        label: 'Customers',
-        key: 'customers',
-        icon: <UserOutlined />,
-      },
-      {
-        label: 'Orders',
-        key: 'orders',
-        icon: <FileTextOutlined />,
-      },
-    ],
-  },
-  {
-    label: 'Sales',
-    key: 'sales',
-    icon: <AreaChartOutlined />,
-    children: [
-      {
-        label: 'Overall',
-        key: 'overall',
-        icon: <LineChartOutlined />,
-      },
-      {
-        label: 'MonthlyData',
-        key: 'monthly-data',
-        icon: <BarChartOutlined />,
-      },
-      {
-        label: 'Breakdown',
-        key: 'breakdown',
-        icon: <PieChartOutlined />,
-      },
-    ],
-  },
-  {
-    label: 'Management',
-    key: 'management',
-    icon: <SafetyCertificateOutlined />,
-    children: [
-      {
-        label: 'Admin',
-        key: 'admin',
-        icon: <SafetyCertificateOutlined />,
-      },
-      {
-        label: 'Performance',
-        key: 'performance',
-        icon: <RiseOutlined />,
-      },
-    ],
-  },
-];
+import { useTranslation } from 'react-i18next';
 
 const DashboardSidebar: FC = () => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
   const [openKeys, setOpenKeys] = useState<string[]>([]);
   const [collapsed, setCollapsed] = useState<boolean>(false);
-  const navigate = useNavigate();
 
   const onOpenChange: MenuProps['onOpenChange'] = keys => {
     const latestOpenKey = keys.find(key => openKeys.indexOf(key) === -1);
@@ -95,13 +29,80 @@ const DashboardSidebar: FC = () => {
   };
 
   return (
-    <Layout.Sider theme="light" collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}>
+    <Layout.Sider theme="light" width={250} collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}>
       <Menu
         mode="inline"
         openKeys={openKeys}
         onOpenChange={onOpenChange}
         onClick={e => navigate(`/dashboard/${e.key}`)}
-        items={DASHBOARD_MENU_ITEMS}
+        items={[
+          {
+            label: t('dashboard'),
+            key: '',
+            icon: <HomeOutlined />,
+          },
+          {
+            label: t('client facing'),
+            key: 'client_facing',
+            icon: <UserOutlined />,
+            children: [
+              {
+                label: t('products'),
+                key: 'products',
+                icon: <ShoppingCartOutlined />,
+              },
+              {
+                label: t('customers'),
+                key: 'customers',
+                icon: <UserOutlined />,
+              },
+              {
+                label: t('orders'),
+                key: 'orders',
+                icon: <FileTextOutlined />,
+              },
+            ],
+          },
+          {
+            label: t('sales'),
+            key: 'sales',
+            icon: <AreaChartOutlined />,
+            children: [
+              {
+                label: t('overall'),
+                key: 'overall',
+                icon: <LineChartOutlined />,
+              },
+              {
+                label: t('monthly data'),
+                key: 'monthly-data',
+                icon: <BarChartOutlined />,
+              },
+              {
+                label: t('breakdown'),
+                key: 'breakdown',
+                icon: <PieChartOutlined />,
+              },
+            ],
+          },
+          {
+            label: t('management'),
+            key: 'management',
+            icon: <SafetyCertificateOutlined />,
+            children: [
+              {
+                label: t('admin'),
+                key: 'admin',
+                icon: <SafetyCertificateOutlined />,
+              },
+              {
+                label: t('performance'),
+                key: 'performance',
+                icon: <RiseOutlined />,
+              },
+            ],
+          },
+        ]}
       />
     </Layout.Sider>
   );
