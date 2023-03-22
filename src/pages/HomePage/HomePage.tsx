d7bea474da3310a3d54cbde00070734d601d8c99
YGOhappy123 <hagiahuy21052003@gmail.com>
2023-03-22 22:50:15 +0700

    Add banner slider to home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import { Carousel } from 'antd';
 import { useTranslation } from 'react-i18next';
 import useTitle from '../../hooks/useTitle';
+import '../../assets/styles/HomePage.css';
+import Banner from './Banner';
 
 export default function RootRoute() {
   const { t } = useTranslation();
   useTitle(`${t('home')} - 7FF`);
 
-  return <div>RootRoute</div>;
+  return (
+    <div className="home-page">
+      <Banner />
+    </div>
+  );
 }
