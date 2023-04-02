2a609a907e9007da66db92398f0e669619ca2f54
Hooannn <hoanthui123@gmail.com>
2023-04-02 18:17:05 +0700

    feat: add gg map

--- src/components/BookingTable.tsx
+++ src/components/BookingTable.tsx
@@ -88,7 +88,16 @@ const BookingTable: FC = () => {
             </Form>
           </div>
           <div className="map-container">
-            <div style={{ height: '100%', background: '#FFBE33' }}>GG MAP</div>
+            <div style={{ height: '100%', background: '#FFBE33' }}>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d979.4379703726116!2d106.60081657554642!3d10.906451778935992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1svi!2s!4v1680434015663!5m2!1svi!2s"
+                width="100%"
+                height="100%"
+                style={{ border: '0' }}
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
           </div>
         </div>
       </div>
