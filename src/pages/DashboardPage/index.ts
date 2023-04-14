7cb1e57301cb6cba5a28b8c543e4f62da07c211a
Nguyen1609 <letrungnguyen331@gmail.com>
2023-04-14 22:15:12 +0700

    tao trang DashboardCategory

--- src/pages/DashboardPage/index.ts
+++ src/pages/DashboardPage/index.ts
@@ -3,4 +3,5 @@ import UsersDashboardPage from './UsersDashboardPage';
 import ProductsDashboardPage from './ProductsDashboardPage';
 import OrdersDashboardPage from './OrdersDashboardPage';
 import VouchersDashboardPage from './VouchersDashboardPage';
-export { DashboardPage, UsersDashboardPage, ProductsDashboardPage, OrdersDashboardPage, VouchersDashboardPage };
+import CategoriesDashboardPage from './CategoriesDashboardPage';
+export { DashboardPage, UsersDashboardPage, ProductsDashboardPage, OrdersDashboardPage, VouchersDashboardPage, CategoriesDashboardPage };
