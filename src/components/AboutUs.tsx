2f0e023ea8c403a1655b068b368d0c4fa30ba556
YGOhappy123 <hagiahuy21052003@gmail.com>
2023-04-20 23:01:01 +0700

    add static content, fix slick css error, embed gg map

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { Button } from 'antd';
 import { containerStyle } from '../assets/styles/globalStyle';
-import { useTranslation } from 'react-i18next';
+import { getI18n, useTranslation } from 'react-i18next';
 import '../assets/styles/components/AboutUs.css';
 import { useNavigate } from 'react-router-dom';
 
@@ -9,9 +9,21 @@ interface IProps {
   isAboutPage?: boolean;
 }
 
+const STORY = {
+  brief: {
+    en: '7FF stands for Seven Fast Food, the name is derived from a group of startups with 7 criteria: Prestige, quality, dedicated service, quick delivery, delicious food, meeting food safety standards products, reasonable prices...',
+    vi: '7FF là viết tắt của Seven Fast Food, cái tên được bắt nguồn từ một nhóm khởi nghiệp với 7 tiêu chí là: Uy tín, chất lượng, phục vụ tận tình, giao hàng nhanh chóng, món ăn ngon, đạt chuẩn an toàn thực phẩm và giá cả hợp lý. 7FF bán chủ yếu là đồ ăn vặt và một số thức ăn nhanh phổ biến...',
+  },
+  full: {
+    en: '7FF stands for Seven Fast Food, the name is derived from a group of startups with 7 criteria: Prestige, quality, dedicated service, quick delivery, delicious food, meeting food safety standards products, reasonable prices. 7FF sells mainly junk food and some popular fast food.\nCurrently, the headquarters of 7FF is in Xuan Thoi Son commune, Hoc Mon district, Ho Chi Minh city, Vietnam. Along with that, there are 7 branches present in major cities, with more than 7000 orders served every day.',
+    vi: '7FF là viết tắt của Seven Fast Food, cái tên được bắt nguồn từ một nhóm khởi nghiệp với 7 tiêu chí là: Uy tín, chất lượng, phục vụ tận tình, giao hàng nhanh chóng, món ăn ngon, đạt chuẩn an toàn thực phẩm và giá cả hợp lý. 7FF bán chủ yếu là đồ ăn vặt và một số thức ăn nhanh phổ biến.\nHiện tại, trụ sở chính của 7FF ở xã Xuân Thới Sơn, huyện Hóc Môn, thành phố Hồ Chí Minh, Việt Nam. Cùng với đó là 7 chi nhánh có mặt ở các thành phố lớn, với hơn 7000 đơn được phục vụ mỗi ngày.',
+  },
+};
+
 const AboutUs: FC<IProps> = ({ isAboutPage }) => {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const locale = getI18n().resolvedLanguage as 'vi' | 'en';
 
   return (
     <section className="about-us">
@@ -21,15 +33,19 @@ const AboutUs: FC<IProps> = ({ isAboutPage }) => {
         </div>
         <div className="story-wrapper">
           <h2 className="heading">{t('we are 7ff')}</h2>
-          <p className="story">
-            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be
-            sure there isn't anything embarrassing hidden in the middle of text. All
-          </p>
-          {!isAboutPage && (
-            <Button type="primary" shape="round" size="large" className="read-more-btn" onClick={() => navigate('/about')}>
-              {t('read more')}
-            </Button>
+          {isAboutPage ? (
+            <>
+              {STORY.full[locale].split('\n').map((prg: string, i: number) => (
+                <p className="story">{prg}</p>
+              ))}
+            </>
+          ) : (
+            <>
+              <p className="story">{STORY.brief[locale]}</p>
+              <Button type="primary" shape="round" size="large" className="read-more-btn" onClick={() => navigate('/about')}>
+                {t('read more')}
+              </Button>
+            </>
           )}
         </div>
       </div>
--- src/pages/HomePage/Banner.tsx
+++ src/pages/HomePage/Banner.tsx
@@ -1,24 +1,79 @@
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import { getI18n, useTranslation } from 'react-i18next';
 import { Button, Carousel } from 'antd';
 import { containerStyle } from '../../assets/styles/globalStyle';
+
+const BANNER_CAROUSEL_CONTENT = [
+  {
+    title: {
+      en: '7 Fast Food restaurant',
+      vi: 'Nhà hàng 7 Fast Food',
+    },
+    description: {
+      en: 'Với 7 tiêu chí đã đề ra, sự  bảo đảm cho từng đơn đặt hàng cho khách, các đầu bếp và nhân viên thông qua tuyển chọn kỹ lưỡng, cùng với sự đa dạng của các món ăn 7FF hứa hẹn sẽ mang đến cho quý khách những sự lựa chọn tốt nhất.',
+      vi: 'Với 7 tiêu chí đã đề ra, sự  bảo đảm cho từng đơn đặt hàng cho khách, các đầu bếp và nhân viên thông qua tuyển chọn kỹ lưỡng, cùng với sự đa dạng của các món ăn 7FF hứa hẹn sẽ mang đến cho quý khách những sự lựa chọn tốt nhất.',
+    },
+    buttonTitle: 'learn more about us',
+    btnNavigation: '/about',
+  },
+  {
+    title: {
+      en: 'Enjoy every bite !',
+      vi: 'Tận hưởng từng miếng cắn !',
+    },
+    description: {
+      en: '7 Fast Food specializes in snacks and popular fast foods.\nWe also serve hot trending dishes on social media platforms, as well as cooling teas that are both refreshing and have many health benefits.',
+      vi: '7 Fast Food chuyên về các món ăn vặt và các loại đồ ăn nhanh phố biến.\nChúng tôi còn có các món hot trend trên các nền tảng mạng xã hội, cũng như các loại trà giải nhiệt vừa giúp giải khát lại có nhiều lợi ích cho sức khỏe.',
+    },
+    buttonTitle: 'see the menu',
+    btnNavigation: '/menu',
+  },
+  {
+    title: {
+      en: 'This month best seller',
+      vi: 'Best seller tháng này',
+    },
+    description: {
+      en: 'Rolled rice paper\nSuper delicious rice paper rolls with quail eggs, with a standard flavor outside the store will surely make you fall in love with the perfect blend of sweet and sour taste.',
+      vi: 'Bánh tráng cuốn\nBánh tráng cuốn trứng cút siêu ngon, với hương vị chuẩn ngoài hàng chắc chắn sẽ khiến bạn thích mê với hương vị chua ngọt hòa quyện hoàn hảo.',
+    },
+    buttonTitle: 'see details',
+    btnNavigation: '/product/64400cbe683d6780bbf7a5e0',
+  },
+];
 
 const Banner: FC = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const locale = getI18n().resolvedLanguage as 'vi' | 'en';
 
   return (
     <section className="banner">
       <img src="/hero-banner.jpg" className="banner-img" />
       <div className="container" style={containerStyle}>
         <Carousel className="carousel" autoplay>
-          <div className="carousel-item-wrapper">
+          {BANNER_CAROUSEL_CONTENT.map((item, i) => (
+            <div key={i} className="carousel-item-wrapper">
+              <div className="carousel-item">
+                <h1 className="header">{item.title[locale]}</h1>
+                {item.description[locale].split('\n').map((prg, _i) => (
+                  <span key={_i} className="description">
+                    {prg}
+                  </span>
+                ))}
+                <Button type="primary" shape="round" size="large" className="order-btn" onClick={() => navigate(item.btnNavigation)}>
+                  {t(item.buttonTitle)}
+                </Button>
+              </div>
+            </div>
+          ))}
+          {/* <div className="carousel-item-wrapper">
             <div className="carousel-item">
-              <h1 className="header">7 Fast Food Restaurant</h1>
+              <h1 className="header">Nhà Hàng 7 Fast Food</h1>
               <span className="description">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores, sint placeat. Iusto nihil eaque molestiae unde, iste ipsam illum
-                minus, quidem eveniet temporibus porro mollitia laborum. Necessitatibus voluptate libero itaque?
+                Với 7 tiêu chí đã đề ra, sự bảo đảm cho từng đơn đặt hàng cho khách, các đầu bếp và nhân viên thông qua tuyển chọn kỹ lưỡng, cùng với
+                sự đa dạng của các món ăn 7FF hứa hẹn sẽ mang đến cho quý khách những sự lựa chọn tốt nhất.
               </span>
               <Button type="primary" shape="round" size="large" className="order-btn" onClick={() => navigate('/about')}>
                 {t('learn more about us')}
@@ -45,7 +100,7 @@ const Banner: FC = () => {
                 {t('order now')}
               </Button>
             </div>
-          </div>
+          </div> */}
         </Carousel>
       </div>
     </section>
